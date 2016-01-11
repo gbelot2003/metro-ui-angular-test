d27d7c2e64ab830230530251f9c92699895e142e
gbelot2003 <gbelot2003@hotmail.com>
2016-01-11 14:31:40 -0600

    first commit, before angularjs

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss').browserify('foundation.js');
+    mix.sass('app.scss').scripts('foundation.js');
 });
