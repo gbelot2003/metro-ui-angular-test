0ab03dcbaddfd671a56c46c479c6e11c7507af2f
gbelot2003 <gbelot2003@hotmail.com>
2016-01-11 16:47:52 -0600

    agregando script de mensajes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -115,5 +115,23 @@
 <div class="notify-container"></div>
 </body>
 <script src="js/all.js"></script>
+<script>
+    function pushMessage(t){
+        var mes = 'Info|Implement independently';
+        $.Notify({
+            caption: mes.split("|")[0],
+            content: mes.split("|")[1],
+            type: t
+        });
+    }
 
+    $(function(){
+        $('.sidebar').on('click', 'li', function(){
+            if (!$(this).hasClass('active')) {
+                $('.sidebar li').removeClass('active');
+                $(this).addClass('active');
+            }
+        })
+    })
+</script>
 </html>
