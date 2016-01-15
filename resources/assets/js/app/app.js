f384963afb3df2b997c9a8ee18c20f6c09957bea
gbelot2003 <gbelot2003@hotmail.com>
2016-01-15 01:42:58 -0600

    cambios en gulpfile, agregando cdn de highchart y jquery, instalando highchart-ng

--- resources/assets/js/app/app.js
+++ resources/assets/js/app/app.js
@@ -6,9 +6,10 @@ require('angular');
 var routes = require('angular-route');
 var animate = require('angular-animate');
 var uibs = require('angular-ui-bootstrap');
+var high = require('highcharts-ng');
 
 /** Main instance **/
-var app = angular.module('adminApp', [routes, animate, uibs]);
+var app = angular.module('adminApp', [routes, animate, uibs, high]);
 
 /** Controllers **/
 var WelcomeCtrl = require('./controllers/WelcomeCtrl');
@@ -36,6 +37,15 @@ app.config(function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(false);
 
 });
+
+app.config(['highchartsNGProvider', function (highchartsNGProvider) {
+    highchartsNGProvider.lazyLoad();// will load hightcharts (and standalone framework if jquery is not present) from code.hightcharts.com
+
+    highchartsNGProvider.lazyLoad([highchartsNGProvider.HIGHCHART/HIGHSTOCK, "maps/modules/map.js", "mapdata/custom/world.js"]);// you may add any additional modules and they will be loaded in the same sequence
+
+    highchartsNGProvider.basePath("/js/"); // change base path for scripts, default is http(s)://code.highcharts.com/
+
+}]);
 
 /** Instances **/
 app.controller('menuController', ['$scope', menuController]);
--- resources/assets/js/app/controllers/ClientesController.js
+++ resources/assets/js/app/controllers/ClientesController.js
@@ -1,7 +1,63 @@
 'use strict';
-var ClientesController = function($scope) {
+var ClientesController = function($scope, highchartsNG) {
     $scope.pageClass = 'page-clientes';
     $scope.testVar = 'Esta es el area de clientes Controller!';
+    $scope.chartConfig = {
+
+        options: {
+            //This is the Main Highcharts chart config. Any Highchart options are valid here.
+            //will be overriden by values specified below.
+            chart: {
+                type: 'bar'
+            },
+            tooltip: {
+                style: {
+                    padding: 10,
+                    fontWeight: 'bold'
+                }
+            }
+        },
+        //The below properties are watched separately for changes.
+
+        //Series object (optional) - a list of series using normal Highcharts series options.
+        series: [{
+            data: [10, 15, 12, 8, 7]
+        }],
+        //Title configuration (optional)
+        title: {
+            text: 'Hello'
+        },
+        //Boolean to control showing loading status on chart (optional)
+        //Could be a string if you want to show specific loading text.
+        loading: false,
+        //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+        //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
+        xAxis: {
+            currentMin: 0,
+            currentMax: 20,
+            title: {text: 'values'}
+        },
+        //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
+        useHighStocks: false,
+        //size (optional) if left out the chart will default to size of the div or something sensible.
+        size: {
+            width: 400,
+            height: 300
+        },
+        //function (optional)
+        func: function (chart) {
+            //setup some logic for the chart
+        }
+    };
+
+    highchartsNG.ready(function(){
+        highchartsNGProvider.lazyLoad();// will load hightcharts (and standalone framework if jquery is not present) from code.hightcharts.com
+
+        highchartsNGProvider.lazyLoad([highchartsNGProvider.HIGHCHART/HIGHSTOCK, "maps/modules/map.js", "mapdata/custom/world.js"]);// you may add any additional modules and they will be loaded in the same sequence
+
+        highchartsNGProvider.basePath("/js/"); // change base path for scripts, default is http(s)://code.highcharts.com/
+
+    },this);
 };
 
 module.exports = ClientesController;
