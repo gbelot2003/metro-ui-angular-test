2cab9fa1086eb8c03ac924b657b27394486e3af7
gbelot2003 <gbelot2003@hotmail.com>
2016-01-15 13:27:35 -0600

    agregando highchart y configuraciones deseadas al sistema!!

--- public/js/app/bundle.js
+++ public/js/app/bundle.js
@@ -42170,11 +42170,24 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
 
 },{}],11:[function(require,module,exports){
 'use strict';
-var ClientesController = function ClientesController($scope, highchartsNG) {
+var ClientesController = function ClientesController($scope, high) {
     $scope.pageClass = 'page-clientes';
     $scope.testVar = 'Esta es el area de clientes Controller!';
-};
 
+    $scope.chartConfig = {
+        options: {
+            chart: {
+                type: 'bar'
+            }
+        },
+        series: [{
+            data: [10, 15, 12, 8, 7]
+        }],
+        title: {
+            text: 'Hello'
+        }
+    };
+};
 module.exports = ClientesController;
 
 },{}],12:[function(require,module,exports){
--- resources/assets/js/app/controllers/ClientesController.js
+++ resources/assets/js/app/controllers/ClientesController.js
@@ -1,7 +1,20 @@
 'use strict';
-var ClientesController = function($scope, highchartsNG) {
+var ClientesController = function($scope, high) {
     $scope.pageClass = 'page-clientes';
     $scope.testVar = 'Esta es el area de clientes Controller!';
-};
 
+    $scope.chartConfig = {
+        options: {
+            chart: {
+                type: 'bar'
+            }
+        },
+        series: [{
+            data: [10, 15, 12, 8, 7]
+        }],
+        title: {
+            text: 'Hello'
+        }
+    };
+};
 module.exports = ClientesController;
